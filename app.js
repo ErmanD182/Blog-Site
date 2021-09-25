4e98f982fd7e6182dbde05d9ca7d4b742a543322
Erman <dins4040@mylaurier.ca>
2021-09-24 20:15:08 -0400

    Added my posts page to only display the user's posts. Text changes to about & contact me pages

--- app.js
+++ app.js
@@ -8,10 +8,6 @@ const session = require("express-session");
 const passport = require("passport");
 const passportLocalMongoose = require("passport-local-mongoose");
 
-const homeStartingContent = "Lacus vel facilisis volutpat est velit egestas dui id ornare. Semper auctor neque vitae tempus quam. Sit amet cursus sit amet dictum sit amet justo. Viverra tellus in hac habitasse. Imperdiet proin fermentum leo vel orci porta. Donec ultrices tincidunt arcu non sodales neque sodales ut. Mattis molestie a iaculis at erat pellentesque adipiscing. Magnis dis parturient montes nascetur ridiculus mus mauris vitae ultricies. Adipiscing elit ut aliquam purus sit amet luctus venenatis lectus. Ultrices vitae auctor eu augue ut lectus arcu bibendum at. Odio euismod lacinia at quis risus sed vulputate odio ut. Cursus mattis molestie a iaculis at erat pellentesque adipiscing.";
-const aboutContent = "Hac habitasse platea dictumst vestibulum rhoncus est pellentesque. Dictumst vestibulum rhoncus est pellentesque elit ullamcorper. Non diam phasellus vestibulum lorem sed. Platea dictumst quisque sagittis purus sit. Egestas sed sed risus pretium quam vulputate dignissim suspendisse. Mauris in aliquam sem fringilla. Semper risus in hendrerit gravida rutrum quisque non tellus orci. Amet massa vitae tortor condimentum lacinia quis vel eros. Enim ut tellus elementum sagittis vitae. Mauris ultrices eros in cursus turpis massa tincidunt dui.";
-const contactContent = "Scelerisque eleifend donec pretium vulputate sapien. Rhoncus urna neque viverra justo nec ultrices. Arcu dui vivamus arcu felis bibendum. Consectetur adipiscing elit duis tristique. Risus viverra adipiscing at in tellus integer feugiat. Sapien nec sagittis aliquam malesuada bibendum arcu vitae. Consequat interdum varius sit amet mattis. Iaculis nunc sed augue lacus. Interdum posuere lorem ipsum dolor sit amet consectetur adipiscing elit. Pulvinar elementum integer enim neque. Ultrices gravida dictum fusce ut placerat orci nulla. Mauris in aliquam sem fringilla ut morbi tincidunt. Tortor posuere ac ut consequat semper viverra nam libero.";
-
 const app = express();
 
 app.set('view engine', 'ejs');
@@ -70,7 +66,7 @@ app.get("/", function(req,res){
   //  console.log("Is authenticated");
     Post.find({}, function(err,posts){
       if(!err){
-        res.render("home", {homeStartingContent: homeStartingContent, posts: posts});
+        res.render("home", {posts: posts});
       }
     });
   }
@@ -82,11 +78,11 @@ app.get("/", function(req,res){
 
 
 app.get("/about", function(req,res){
-  res.render("about", {aboutContent: aboutContent});
+  res.render("about", {loggedIn: req.isAuthenticated() ? true : false});
 });
 
 app.get("/contact", function(req,res){
-  res.render("contact", {contactContent: contactContent});
+  res.render("contact", {loggedIn: req.isAuthenticated() ? true : false});
 });
 
 app.get("/compose", function(req,res){
@@ -113,12 +109,17 @@ app.get("/signout", function(req, res){
 
 app.get("/posts/:id", function(req,res){
   const requestId = req.params.id;
+  let showDeleteBtn = false;
 
   if(req.isAuthenticated()){
     Post.findOne({_id: requestId}, function(err, foundPost){
       if(!err){
         currentPostId = requestId;
-        res.render("post", {postTitle: foundPost.title, postContent: foundPost.content, postDate: foundPost.date, userName: foundPost.user});
+
+        if(foundPost.user === req.user.name){
+          showDeleteBtn = true;
+        }
+        res.render("post", {postTitle: foundPost.title, postContent: foundPost.content, postDate: foundPost.date, userName: foundPost.user, check: showDeleteBtn});
       }
     });
   }
@@ -126,9 +127,19 @@ app.get("/posts/:id", function(req,res){
   else{
     res.redirect("/login");
   }
+});
 
-
-
+app.get("/my-posts", function(req,res){
+  if(req.isAuthenticated()){
+    Post.find({user: req.user.name}, function(err,posts){
+      if(!err){
+        res.render("my-posts", {posts: posts});
+      }
+    });
+  }
+  else{
+    res.redirect("/login");
+  }
 });
 
 app.post("/back", function(req,res){
@@ -148,6 +159,14 @@ app.post("/compose", function(req,res){
       user: req.user.name
   });
 
+  const posts = req.user.posts;
+  posts.push(newPost);
+
+  User.updateOne({_id: req.user._id},{posts: posts}, function(err){
+    if(err){
+      console.log(err);
+    }
+  });
 
   newPost.save(function(err){
     if(!err){
@@ -161,10 +180,22 @@ app.post("/btn", function(req,res){
   res.redirect("/compose");
 });
 
+
+//TODO: NEED TO DELETE POSTS FROM USER's POSTS
 app.post("/delete", function(req,res){
   Post.findByIdAndRemove(currentPostId, function(err){
     if(!err){
-      res.redirect("/");
+      User.findByIdAndUpdate(
+        {_id: req.user._id},
+        {$pull: {posts: { _mcid: currentPostId }}}, function(err){
+        if(!err){
+          res.redirect("/");
+        }
+        else{
+          console.log(err);
+        }
+      });
+
     }
   });
 
